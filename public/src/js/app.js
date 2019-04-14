e7b8ce1dd7ef686b9d1191419e38ff7057d658ab
Kacper Ostrowski <kacper.j.ostrowski@gmail.com>
2019-04-14 19:42:29 +0200

    added fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,6 +5,9 @@ if ('serviceWorker' in navigator) {
     .register('/sw.js')
     .then(function() {
         console.log('Service worker registred   ');
+    })
+    .catch(function(err){
+        console.log('error: ', err);
     });
 }
 
@@ -14,3 +17,44 @@ window.addEventListener('beforeinstallprompt', function(event) {
     defferedPrompt = event;
     return false;
 });
+
+var promise = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        //resolve("This is executed once the timer is done@");
+        reject({code: 500, message: "An error occured!"});
+    },3000);
+});
+
+
+fetch('https://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        },
+        body: JSON.stringify({
+            message: 'Does this work?'
+        }),
+        mode: 'cors'
+    })
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+promise.then(function(text){
+    return text;
+}).then(function(newText){
+    console.log('2: ');
+})
+.catch(function(err){
+    console.log('catch', err.code, err.message);
+});
+
+console.log('This is executed right after setTimeout');
